09392b02b8816c73137006893717b90878fd34af
wear工程师 <63706387+wearzdk@users.noreply.github.com>
2025-08-21 14:46:22 +0800

    Update SideRight.js

--- themes/heo/components/SideRight.js
+++ themes/heo/components/SideRight.js
@@ -34,7 +34,7 @@ export default function SideRight(props) {
 
   return (
     <div id='sideRight' className='hidden xl:block w-72 space-y-4 h-full'>
-      <InfoCard {...props} className='w-72 wow fadeInUp' />
+      // <InfoCard {...props} className='w-72 wow fadeInUp' />
 
       <div className='sticky top-20 space-y-4'>
         {/* 文章页显示目录 */}
